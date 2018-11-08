a0f8f85d52229a74ad3c8acb6047c5ad5efff84e
Xiliusha <40131112+Xiliusha@users.noreply.github.com>
2018-11-08 15:37:03 +0800

    Update game/Thlib/enemy/boss.lua

--- game/Thlib/enemy/boss.lua
+++ game/Thlib/enemy/boss.lua
@@ -757,7 +757,7 @@ function boss.move:init()
 end
 function boss.move:del() end
 
-boss.escape={}
+boss.escape={}--这个和boss.move有什么区别？？？
 
 function boss.escape.New(x,y,t,m)
 	local c={}
@@ -796,16 +796,6 @@ boss_ui=Class(object)
 
 boss_ui.active_count=0
 
-function boss:SetUIDisplay(hp,name,cd,spell,pos,pointer)
-	self.ui.drawhp=hp
-	self.ui.drawname=name
-	self.ui.drawtime=cd
-	self.ui.drawspell=spell
-	self.ui.needposition=pos
-	self.ui.drawpointer=pointer or 1
-end
-
-
 function boss_ui:init(b)
 	self.layer=LAYER_TOP
 	self.group=GROUP_GHOST
@@ -822,85 +812,42 @@ function boss_ui:init(b)
 	if b.class.sc_image then self.sc_image = New(b.class.sc_image) end
 end
 
-function test_ex(ex)
-	ex.lifes={300,100,400}
-	ex.lifesmax={300,100,700}
-	ex.modes={0,1,0}
+function boss_ui:frame()
+	task.Do(self)
+	if self.countdown then
+		if self.countdown>5 and self.countdown<=10 and self.countdown%1==0 then PlaySound('timeout',0.6) end
+		if self.countdown>0 and self.countdown<=5 and self.countdown%1==0 then PlaySound('timeout2',0.8) Print(self.timer) end
+	end
 end
 
-
 function boss_ui:render()
+	SetViewMode'world'
+	
 	local yn=boss_ui.active_count
 	local dy1=yn*36
 	local dy2=yn*44
 	if self.needposition then
 	boss_ui.active_count=boss_ui.active_count+1
 	end
-	
-	SetViewMode'world'
 	local _dy=0
 	local alpha1=1-self.boss.hp_flag/30
 	
-	
-	--SetImageState('base_hp','',Color(alpha1*255,255,0,0))
 	SetImageState('base_hp','',Color(alpha1*255,255,0,0))
 	SetImageState('hpbar1','',Color(alpha1*255,255,255,255))
 	SetImageState('hpbar2','',Color(0,255,255,255))
 	SetImageState('life_node','',Color(alpha1*255,255,255,255))
+	--血条、boss名、剩余符卡
 	if self.boss.ex then
-		if self.boss.ex.cardcount and self.drawhp then	
+		if self.boss.ex.cardcount and self.drawhp then
 			boss_ui.render_hpbar_ex(self)
 		end
 	end
---	RenderText('bonus',self.ex.status,0,207,0.5,'right')
+	--RenderText('bonus',self.ex.status,0,207,0.5,'right')
 	if self.is_combat then
 		if self.hpbarlen or self.boss.ex then
-			if not(self.boss.time_sc) and self.drawhp then
-				if self.boss.ex then
-					--if self.boss.ex.cardcount then	
-					--	boss_ui.render_hpbar_ex(self)
-					--end
-				elseif not(self.hpbarcolor2) then  --  sp-'sp'
-					misc.Renderhpbar(self.boss.x,self.boss.y,90,360,60,64,360,1)
-					misc.Renderhp(self.boss.x,self.boss.y,90,360,60,64,360,self.hpbarlen*min(1,self.boss.timer/60))
-					Render('base_hp',self.boss.x,self.boss.y,0,0.274,0.274)
-					Render('base_hp',self.boss.x,self.boss.y,0,0.256,0.256)
-					if self.boss.sp_point and #self.boss.sp_point~=0 then
-						for i=1,#self.boss.sp_point do
-							Render('life_node',self.boss.x+61*cos(self.boss.sp_point[i]),self.boss.y+61*sin(self.boss.sp_point[i]),self.boss.sp_point[i]-90,0.5)
-						end
-					end
-				elseif not(self.hpbarcolor1) then  --'non'-non
-					misc.Renderhpbar(self.boss.x,self.boss.y,90,360,60,64,360,1)
-					misc.Renderhp(self.boss.x,self.boss.y,90,360,60,64,360,self.hpbarlen*min(1,self.boss.timer/60))
-					Render('base_hp',self.boss.x,self.boss.y,0,0.274,0.274)
-					Render('base_hp',self.boss.x,self.boss.y,0,0.256,0.256)
-				elseif self.hpbarcolor1==self.hpbarcolor2 then  --non-'sp'
-					misc.Renderhpbar(self.boss.x,self.boss.y,90,360,60,64,360,1)
-					misc.Renderhp(self.boss.x,self.boss.y,90,self.boss.lifepoint-90,60,64,self.boss.lifepoint-88,self.hpbarlen)
-					Render('base_hp',self.boss.x,self.boss.y,0,0.274,0.274)
-					Render('base_hp',self.boss.x,self.boss.y,0,0.256,0.256)
-				elseif self.hpbarcolor1~=self.hpbarcolor2 then  --'non'-sp
-					misc.Renderhpbar(self.boss.x,self.boss.y,90,360,60,64,360,1)
-					if self.boss.timer<=60 then
-						misc.Renderhp(self.boss.x,self.boss.y,90,360,60,64,360,self.hpbarlen*min(1,self.boss.timer/60))
-					else
-						misc.Renderhp(self.boss.x,self.boss.y,90,self.boss.lifepoint-90,60,64,self.boss.lifepoint-88,1)
-						misc.Renderhp(self.boss.x,self.boss.y,self.boss.lifepoint,450-self.boss.lifepoint,60,64,450-self.boss.lifepoint,self.hpbarlen)
-					end
-					Render('base_hp',self.boss.x,self.boss.y,0,0.274,0.274)
-					Render('base_hp',self.boss.x,self.boss.y,0,0.256,0.256)
-					Render('life_node',self.boss.x+61*cos(self.boss.lifepoint),self.boss.y+61*sin(self.boss.lifepoint),self.boss.lifepoint-90,0.55)
-					SetFontState('bonus','',Color(255,255,255,255))
-				end
-				if self.boss.show_hp then
-				SetFontState('bonus','',Color(255,0,0,0))
-				RenderText('bonus',int(max(0,self.boss.hp))..'/'..self.boss.maxhp,self.boss.x-1,self.boss.y-40-1,0.6,'centerpoint')
-				SetFontState('bonus','',Color(255,255,255,255))
-				RenderText('bonus',int(max(0,self.boss.hp))..'/'..self.boss.maxhp,self.boss.x,self.boss.y-40,0.6,'centerpoint')
-				end
-			end
-		---------------------------
+			--boss hpbar
+			boss_ui.render_hpbar(self)
+			--boss name and cards left
 			if self.drawname then
 				RenderTTF('boss_name',self.name,-185,-185,222-dy1,222-dy1,Color(0xFF000000),'noclip')
 				RenderTTF('boss_name',self.name,-186,-186,223-dy1,223-dy1,Color(0xFF80FF80),'noclip')
@@ -918,18 +865,19 @@ function boss_ui:render()
 			end
 		end
 	end
+	--位置指示器
 	if self.pointer_x and self.drawpointer then
 		SetViewMode'ui'
 		Render('boss_pointer',WorldToScreen(max(min(self.pointer_x,lstg.world.r-24),lstg.world.l+24),lstg.world.b))--适配宽屏
 		SetViewMode'world'
 	end
-	
+	--为boss ex时，下方逻辑不执行
 	if self.boss.ex then
 		if self.boss.ex.status==0 then
-			return 
+			return
 		end
 	end
-	
+	--透明度和位置处理
 	local axy=0
 	for _,p in pairs(Players(self)) do
 		local ax,ay=0,0
@@ -943,6 +891,7 @@ function boss_ui:render()
 	SetFontState('time','',Color(alpha*255,255,255,255))
 	local xoffset=384
 	if self.boss.sc_bonus then xoffset=max(384-self.boss.timer*7,0) else xoffset=min(384,(self.boss.timer+1)*7) end
+	--符卡
 	if self.drawspell then
 		if self.sc_name~='' then
 			SetImageState('boss_spell_name_bg','',Color(alpha*255,255,255,255))
@@ -950,7 +899,6 @@ function boss_ui:render()
 			RenderTTF('sc_name',self.sc_name,193+xoffset,193+xoffset,226-dy2,226-dy2,Color(alpha*255,0,0,0),'right','noclip')
 			RenderTTF('sc_name',self.sc_name,192+xoffset,192+xoffset,227-dy2,227-dy2,Color(alpha*255,255,255,255),'right','noclip')
 		end
-
 		if self.boss.sc_bonus and self.sc_hist then
 			local b
 			if self.boss.sc_bonus>0 then b=string.format('%.0f',self.boss.sc_bonus-self.boss.sc_bonus%10) else b='FAILED ' end
@@ -962,27 +910,65 @@ function boss_ui:render()
 			RenderText('bonus',b,186+xoffset,208-dy2,0.5,'right')
 			RenderText('bonus',string.format('%d/%d',self.sc_hist[1],self.sc_hist[2]),96+xoffset,208-dy2,0.5,'right')
 			RenderText('bonus','HISTORY	   BONUS',136+xoffset,208-dy2,0.5,'right')
-		end	
+		end
 	end
+	--非符、符卡时间
 	if self.is_combat and self.drawtime then
 		local cd=(self.countdown-int(self.countdown))*100
 		local yoffset
 		if self.boss.sc_bonus then yoffset=max(20-self.boss.timer,0) else yoffset=min(20,(self.boss.timer+1)) end
 		if self.countdown>=10.0 then
-		--	RenderText('score',string.format('%.2f',min(99.99,self.countdown)),0,188,0.5,'centerpoint')
---			SetFontState('time','',Color(alpha1*255,0,0,0))
---			RenderText('time',string.format('%d',min(180.99,int(self.countdown))),0,193+yoffset+_dy,0.5,'centerpoint')
---			RenderText('time','			.'..string.format('%d%d',min(9,cd/10),min(9,cd%10)),0,190+yoffset+_dy,0.3,'centerpoint')
 			SetFontState('time','',Color(alpha1*255,255,255,255))
 			RenderText('time',string.format('%d',int(self.countdown)),4,192+yoffset+_dy-dy2,0.5,'vcenter','right')
 			RenderText('time','.'..string.format('%d%d',min(9,cd/10),min(9,cd%10)),4,189+yoffset+_dy-dy2,0.3,'vcenter','left')
 		else
---			SetFontState('time','',Color(alpha1*255,0,0,0))
---			RenderText('time',string.format('0%d',min(99.99,int(self.countdown))),0,193+yoffset+_dy,0.5,'centerpoint')
---			RenderText('time','			.'..string.format('%d%d',min(9,cd/10),min(9,cd%10)),0,190+yoffset+_dy,0.3,'centerpoint')
 			SetFontState('time','',Color(alpha1*255,255,30,30))
 			RenderText('time',string.format('0%d',min(99.99,int(self.countdown))),4,192+yoffset+_dy-dy2,0.5,'vcenter','right')
 			RenderText('time','.'..string.format('%d%d',min(9,cd/10),min(9,cd%10)),4,189+yoffset+_dy-dy2,0.3,'vcenter','left')
+		end
+	end
+end
+
+function boss_ui:render_hpbar()
+	if not(self.boss.time_sc) and self.drawhp then
+		if not(self.hpbarcolor2) then--sp-'sp'
+			misc.Renderhpbar(self.boss.x,self.boss.y,90,360,60,64,360,1)
+			misc.Renderhp(self.boss.x,self.boss.y,90,360,60,64,360,self.hpbarlen*min(1,self.boss.timer/60))
+			Render('base_hp',self.boss.x,self.boss.y,0,0.274,0.274)
+			Render('base_hp',self.boss.x,self.boss.y,0,0.256,0.256)
+			if self.boss.sp_point and #self.boss.sp_point~=0 then
+				for i=1,#self.boss.sp_point do
+					Render('life_node',self.boss.x+61*cos(self.boss.sp_point[i]),self.boss.y+61*sin(self.boss.sp_point[i]),self.boss.sp_point[i]-90,0.5)
+				end
+			end
+		elseif not(self.hpbarcolor1) then--'non'-non
+			misc.Renderhpbar(self.boss.x,self.boss.y,90,360,60,64,360,1)
+			misc.Renderhp(self.boss.x,self.boss.y,90,360,60,64,360,self.hpbarlen*min(1,self.boss.timer/60))
+			Render('base_hp',self.boss.x,self.boss.y,0,0.274,0.274)
+			Render('base_hp',self.boss.x,self.boss.y,0,0.256,0.256)
+		elseif self.hpbarcolor1==self.hpbarcolor2 then--non-'sp'
+			misc.Renderhpbar(self.boss.x,self.boss.y,90,360,60,64,360,1)
+			misc.Renderhp(self.boss.x,self.boss.y,90,self.boss.lifepoint-90,60,64,self.boss.lifepoint-88,self.hpbarlen)
+			Render('base_hp',self.boss.x,self.boss.y,0,0.274,0.274)
+			Render('base_hp',self.boss.x,self.boss.y,0,0.256,0.256)
+		elseif self.hpbarcolor1~=self.hpbarcolor2 then--'non'-sp
+			misc.Renderhpbar(self.boss.x,self.boss.y,90,360,60,64,360,1)
+			if self.boss.timer<=60 then
+				misc.Renderhp(self.boss.x,self.boss.y,90,360,60,64,360,self.hpbarlen*min(1,self.boss.timer/60))
+			else
+				misc.Renderhp(self.boss.x,self.boss.y,90,self.boss.lifepoint-90,60,64,self.boss.lifepoint-88,1)
+				misc.Renderhp(self.boss.x,self.boss.y,self.boss.lifepoint,450-self.boss.lifepoint,60,64,450-self.boss.lifepoint,self.hpbarlen)
+			end
+			Render('base_hp',self.boss.x,self.boss.y,0,0.274,0.274)
+			Render('base_hp',self.boss.x,self.boss.y,0,0.256,0.256)
+			Render('life_node',self.boss.x+61*cos(self.boss.lifepoint),self.boss.y+61*sin(self.boss.lifepoint),self.boss.lifepoint-90,0.55)
+			SetFontState('bonus','',Color(255,255,255,255))
+		end
+		if self.boss.show_hp then
+		SetFontState('bonus','',Color(255,0,0,0))
+		RenderText('bonus',int(max(0,self.boss.hp))..'/'..self.boss.maxhp,self.boss.x-1,self.boss.y-40-1,0.6,'centerpoint')
+		SetFontState('bonus','',Color(255,255,255,255))
+		RenderText('bonus',int(max(0,self.boss.hp))..'/'..self.boss.maxhp,self.boss.x,self.boss.y-40,0.6,'centerpoint')
 		end
 	end
 end
@@ -993,7 +979,7 @@ function boss_ui:render_hpbar_ex()
 	local alpha1=1-self.boss.hp_flag/30
 	
 	local ex=self.boss.ex
-	if ex==nil then	return end
+	if ex==nil then return end
 	local lifes=ex.lifes
 	local lifesmax=ex.lifesmax
 	local modes=ex.modes
@@ -1006,10 +992,10 @@ function boss_ui:render_hpbar_ex()
 	local maxlife=0
 	local life=0
 	for i, z in ipairs(lifesmax) do
-	   maxlife=maxlife+z
+		maxlife=maxlife+z
 	end
 	for i, z in ipairs(lifes) do
-	   life=life+z
+		life=life+z
 	end
 	life=min(maxlife*rate,life)
 
@@ -1060,19 +1046,25 @@ function boss_ui:render_hpbar_ex()
 	
 end
 
-
-function boss_ui:frame()
-	task.Do(self)
-	if self.countdown then
-		if self.countdown>5 and self.countdown<=10 and self.countdown%1==0 then PlaySound('timeout',0.6) end
-		if self.countdown>0 and self.countdown<=5 and self.countdown%1==0 then PlaySound('timeout2',0.8) Print(self.timer) end
-	end
-end
-
-function boss_ui:kill()
+function boss_ui:kill()--kill and del function
 	Del(self.sc_image)
 	boss_ui.active_count=boss_ui.active_count-1
 end boss_ui.del=boss_ui.kill
+
+function boss:SetUIDisplay(hp,name,cd,spell,pos,pointer)
+	self.ui.drawhp=hp
+	self.ui.drawname=name
+	self.ui.drawtime=cd
+	self.ui.drawspell=spell
+	self.ui.needposition=pos
+	self.ui.drawpointer=pointer or 1
+end
+
+function test_ex(ex)--测试代码，日后要移除
+	ex.lifes={300,100,400}
+	ex.lifesmax={300,100,700}
+	ex.modes={0,1,0}
+end
 
 ----------------------------------------
 --boss 特效
